5954a6aed4cf277a5b09dc3bb0e0be00a74ebfe6
qlilp <58059731+qlilp@users.noreply.github.com>
2024-09-13 19:40:06 +0800

    Create kuaishou_jisu_baoxiang2.py

---
+++ kuaishou_jisu_baoxiang2.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+# coding=utf-8
+import sys
+import os
+import traceback
+import requests
+import json
+
+import hashlib
+
+SIGN_LOG = 'logs/kuaishou_sign2.log'
+
+work_path = os.path.dirname(os.path.abspath(__file__))
+SIGN_LOG_FILE = os.path.join(work_path, SIGN_LOG)
+
+
+def get_baoxiang(token, __NS_sig3):
+    print('💎💎💎💎开始领取宝箱💎💎💎💎')
+    access_token = ''
+    try:
+        url = "https://nebula.kuaishou.com/rest/wd/encourage/unionTask/treasureBox/report?__NS_sig3=" + __NS_sig3 + "&sigCatVer=1"
+
+        # 定义请求头
+        headers = {
+            "Host": "nebula.kuaishou.com",
+            "Connection": "keep-alive",
+            "Content-Length": "2",
+            "User-Agent": "Mozilla/5.0 (Linux; Android 14; 23113RKC6C Build/UKQ1.230804.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.226 KsWebView/1.8.90.675 (rel) Mobile Safari/537.36 Yoda/3.1.7-alpha33-intercept1 ksNebula/12.5.20.8014 OS_PRO_BIT/64 MAX_PHY_MEM/15199 AZPREFIX/az4 ICFO/0 StatusHT/34 TitleHT/43 NetType/WIFI ISLP/0 ISDM/0 ISLB/0 locale/zh-cn DPS/19.822 DPP/99 CT/0 ISLM/0",
+            "content-type": "application/json",
+            "Accept": "*/*",
+            "Origin": "https://nebula.kuaishou.com",
+            "X-Requested-With": "com.kuaishou.nebula",
+            "Sec-Fetch-Site": "same-origin",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Dest": "empty",
+            "Referer": "https://nebula.kuaishou.com/nebula/task/earning?source=timer&layoutType=4&hyId=nebula_earning",
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+            "Cookie": token
+        }
+
+        # 发送 POST 请求
+        resp = requests.post(url, headers=headers, data=json.dumps({}))
+        resp_json = resp.json()
+        if resp_json['result'] == 1:
+            title_reward_count = resp_json['data']['title']['rewardCount']
+            print(f"得到金币：{title_reward_count}")
+        else:
+            print(resp_json['error_msg'])
+    except:
+        print(f"获取异常:{traceback.format_exc()}")
+        
+
+    return access_token
+
+
+# 获取环境变量
+_cookie = os.getenv('KSJSB_COOKIE2')
+
+# 检查变量是否存在
+if _cookie == '':
+    print("请先在环境变量里添加 \"KS_COOKIE\" 填写对应快手的 cookie 值")
+    exit(0)
+
+def gen_tokensig(sig,salt=""):
+    v = sig + salt
+    return hashlib.sha256(v.encode('utf-8')).hexdigest()
+
+def gen_sig(params,data):
+    dd = dict(params,**data)
+    dict_sort_res = dict(sorted(dd.items(),key=lambda x:x[0]))
+    ss = ""
+    for key,value in dict_sort_res.items():
+        if key not in ["sig","__NS_sig3","sig2"]:
+            ss += f"{key}={value}"
+    ss += "ca8e86efb32e"
+    return hashlib.md5(ss.encode()).hexdigest()
+
+
+
+def main():
+    get_baoxiang(_cookie, "405017275e6092f58d1c831f18193423a38e57d583e79302df4e0f0f09090a0b3414")
+    
+if __name__ == '__main__':
+    main()
